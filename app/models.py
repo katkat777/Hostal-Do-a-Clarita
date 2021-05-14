f9e0a8fc3f88a9318f9f1b91f8fc926c59fb2518
Elglitches <ivanduranluengo@gmail.com>
2021-05-13 20:31:33 -0400

    base de datos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    rut = models.CharField(max_length=11, unique=True)
+    nombre = models.CharField(max_length=50)
+    apellido =models.CharField(max_length=50)
+

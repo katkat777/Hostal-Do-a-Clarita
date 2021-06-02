94056b0772cd48ad9829c875a53834c4154a9efd
matiarom3 <matiasroignacio300@gmail.com>
2021-06-01 22:47:40 -0400

    registro de producto

--- app/views.py
+++ app/views.py
@@ -14,6 +14,9 @@ def contacto(request):
 def galeria(request):
     return render(request, 'app/galeria.html')
 
+
+
+
 def registrohabitacion(request):
     data = {
         'habitaciones':listado_habitaciones(),
@@ -57,6 +60,8 @@ def agregar_habitacion(id_habitacion, precio, tipo_cama, caracteristicas, reserv
     return salida.getvalue()
 
     
+
+
 
 def registro(request):
     data = {
@@ -102,19 +107,21 @@ def agregar_cliente(rut, id_cliente, emp_cliente, id_huesped):
 
 
 
+
+
 def RegistroProducto(request):
     data = {
-        'habitaciones':listado_productos(),
+        'productos':listado_productos(),
     }
     if request.method == 'POST':
         id_producto = request.POST.get('id_producto')
         precio = request.POST.get('precio')
         tipo_producto = request.POST.get('tipo_producto')
         stock = request.POST.get('stock')
-        stock_critico = request.POST.get('reserva_id_reserva')
-        fech_venc = request.POST.get('accesorios')
+        stock_critico = request.POST.get('stock_critico')
+        fech_venc = request.POST.get('fech_venc')
         descripcion = request.POST.get('descripcion')
-        salida = agregar_habitacion(id_producto, precio, tipo_producto, stock, stock_critico, fech_venc, descripcion)
+        salida = agregar_producto(id_producto, precio, tipo_producto, stock, stock_critico, fech_venc, descripcion)
         if salida == 1:
             data['mensaje'] = 'agregado correctamente'
             data['productos'] = listado_productos()
@@ -122,16 +129,19 @@ def RegistroProducto(request):
             data['mensaje'] = 'no se pudo guardar'
 
     return render(request, 'app/RegistroProducto.html', data)
+
+
     
 def agregar_producto(id_producto, precio, tipo_producto, stock, stock_critico, fech_venc, descripcion):
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_AGREGAR_PRODUCTOS',[id_producto, precio, tipo_producto, stock, stock_critico, fech_venc, descripcion])
+    cursor.callproc('SP_AGREGAR_PRODUCTO',[id_producto, precio, tipo_producto, stock, stock_critico, fech_venc, descripcion])
     return salida.getvalue()
 
 
     
+
 def listado_productos():
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
